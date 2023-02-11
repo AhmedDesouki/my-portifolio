3545d2d840418127c3f43aa2242b5047f6cc6ee2
AhmedDesouki <ahmed195141@bue.edu.eg>
2023-02-12 00:00:17 +0200

    second day

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>DESOUKIII's PORTIFOLIO</title>
       </Head>
 
       <Header />
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
       {/* Hero  */}
